24a0ca5334a4bca0c3f54ea3e30a5260ee9c71ac
Mack Grissom <mcintyregrissom@gmail.com>
2023-02-12 23:12:22 -0500

    added particles

--- src/script.js
+++ src/script.js
@@ -1,6 +1,23 @@
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
 
+
+// spotlight effect
+// const spotlightEl = document.querySelector("#spotlight");
+
+// function handleMouseMove(event) {
+//     const { clientX, clientY } = event;
+
+//     spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 10px, #000000ee 350px)`;
+// }
+
+// document.addEventListener("mousemove", handleMouseMove)
+
+
+
+
+
+
 /**
  * Debug
  */
@@ -28,7 +45,7 @@ const scene = new THREE.Scene()
 
 // texture
 const textureLoader = new THREE.TextureLoader()
-const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+const gradientTexture = textureLoader.load('textures/gradients/5.jpg')
 gradientTexture.magFilter = THREE.NearestFilter
 
 // Material
@@ -55,7 +72,9 @@ const mesh3 = new THREE.Mesh(
 mesh1.position.y = - objectsDistance * 0
 mesh2.position.y = - objectsDistance * 1 
 mesh3.position.y = - objectsDistance * 2 
-
+mesh1.position.x = 2
+mesh2.position.x = -2
+mesh3.position.x = 2
 
 scene.add(mesh1, mesh2, mesh3)
 
@@ -63,6 +82,32 @@ const  sectionMeshes = [mesh1, mesh2, mesh3]
 
 
 
+// particles
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+
+for(let i = 0; i < particlesCount; i++)
+{
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+    positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random()  * 10
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+
+// particle geometry
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+// particle material
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor, 
+    sizeAttenuation: true, 
+    size: 0.03
+})
+// particle points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+
+// light
 const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
 directionalLight.position.set(1,1,0)
 scene.add(directionalLight)
@@ -91,13 +136,20 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+
+
 /**
  * Camera
  */
+
+// group 
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -119,17 +171,35 @@ window.addEventListener('scroll', () => {
 
 })
 
+// cursor
+const cursor = {}
+cursor.x=0
+cursor.y=0
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height -0.5
+})
+
+
 /**
  * Animate
  */
 const clock = new THREE.Clock()
-
+let previousTime = 0
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
+const deltaTime = elapsedTime - previousTime
+previousTime = elapsedTime
     // Animate camera
-camera.position.y = - scrollY
+camera.position.y = - scrollY / sizes.height * objectsDistance
+const parallaxX = cursor.x * 0.5
+const parallaxY = - cursor.y * 0.5
+
+cameraGroup.position.x +=  (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+cameraGroup.position.y +=  (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+
 
 // animate meshes
 for(const mesh of sectionMeshes) {
